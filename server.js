0eb70ede9615b7aac559948a8343ca722341edfd
jaytai2040 <jayyr2040@gmail.com>
2021-07-23 20:06:06 +0800

    heroku deploy

--- server.js
+++ server.js
@@ -4,32 +4,27 @@ const app = express();
 const PORT = process.env.PORT;
 const mongoose = require("mongoose");
 const MONGODB_URI = process.env.MONGODB_URI;
-const path = require('path');
 
 // Mongoose connection
 mongoose.connection.on("error", (err) =>
   console.log(err.message + " is Mongod not running?")
 );
 mongoose.connection.on("disconnected", () => console.log("mongo disconnected"));
- 
-//...farther down the page
- 
+
 mongoose.connect(MONGODB_URI, {
-useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
 });
 
 mongoose.connection.once("open", () => {
-    console.log("connected to mongoose...");
-  });
+  console.log("connected to mongoose...");
+});
 
 // Middleware
-app.use(express.static('./front_end/build')); 
-app.use(express.json()); 
-
-const holidaysController = require("./controllers/holidays.js");
-app.use("/holidays", holidaysController);
+app.use(express.json());
 
 app.listen(PORT, () => {
-    console.log("🎉🎊", "matching happening on port", PORT, "🎉🎊");
-  });
-  
+  console.log("Matching happening on port", PORT);
+});
